7f48e38c2d7d2c2b57e39c72b9b2950916f0f557
Leonardo Freires <leonardo_sf7@hotmail.com>
2019-05-15 04:06:43 -0300

    Finalizar template da pag feed

--- demonday/static/script/script.js
+++ demonday/static/script/script.js
@@ -1,23 +1,35 @@
-let divCadastro = document.querySelector('article div > div:first-child');
-let divLogin = document.querySelector('article div > div:last-child');
-let botaoCadastro = document.querySelector('.cadastro input:nth-child(3)');
-let botaoLogin = document.querySelector('.login input:nth-child(3)');
+// Animação do botão e Collapsible do novo post.
 let newPostButton = document.querySelector('.newPostButton');
 let spanVertical = document.querySelector('.spanVertical');
-let spanHorizontal = document.querySelector('.spanHorizontal');
-let newPost = document.querySelector('.newPost')
-
-function ligarDiv(){
-    divCadastro.classList.toggle('ligou');
-    divLogin.classList.toggle('ligou');
-}
 
 function animacaoNewPost(){
     spanVertical.classList.toggle('verticalDesce');
-    newPost.classList.toggle('activeNewPost');
+    newPostButton.classList.toggle('active');
+    let content = document.querySelector('.newPost');
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + 'px';
+    }
 }
 
-botaoCadastro.onclick = ligarDiv();
-botaoLogin.onclick = ligarDiv();
-
 newPostButton.onclick = animacaoNewPost;
+
+
+// Troca de abas do feed
+
+function openTab(evt, tabName) {
+    let i, tab, tabContent;
+    tabContent = document.querySelectorAll('.tabContent');
+    tab = document.querySelectorAll('.tab');
+    for (i=0; i<tabContent.length; i++) {
+        tabContent[i].style.display = 'none';
+    }
+    for (i=0; i<tab.length; i++) {
+        tab[i].className = tab[i].className.replace('active', '');
+    }
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " active";
+}
+
+document.querySelector('#defaultOpen').click();
